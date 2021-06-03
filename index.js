a696b7aa235ca7c88e658f65ce17ed2a2bf145ea
john <john.d.leonetti@gmail.com>
2021-06-03 13:13:07 -0700

    commit tree 6e49d467b08cb909dc400712e6ba41b15d5cc170

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// make new variable
+// go through each element and start reducing elements
+// return only single items
+
+let totalBatteries = batteryBatches.reduce(function(total, numberBatteries){
+    return numberBatteries + total
+})
